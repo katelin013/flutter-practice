9cab37ab4b397348f22887a5e986574b287d27ee
kayle <kayle@nextlink.com.tw>
2020-05-08 18:14:21 +0800

    push_code

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// 載入 material 庫
+import 'package:flutter/material.dart';
+
+// 進入點
+// runApp 是 Flutter 進入點
+void main() { runApp(
+  new Center(
+    child: new Text(
+      'First app',
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
