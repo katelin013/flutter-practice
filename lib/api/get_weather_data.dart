a434016a4714de419a5f7aa4b573f2fbbcba83a6
kayle <kayle@nextlink.com.tw>
2020-05-14 11:33:00 +0800

    first_upload_project

--- lib/api/get_weather_data.dart
+++ lib/api/get_weather_data.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutterwidgetexample/api/http_utils.dart';
+import 'package:flutterwidgetexample/utils/http_utils.dart';
 import 'package:flutterwidgetexample/config.dart';
-import 'package:dio/dio.dart';
 import 'package:flutterwidgetexample/entity/weather_result_entity.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
 
-class GetWeatherData extends StatelessWidget{
+class GetWeatherData extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
+        return MultiProvider(
+            providers: [ChangeNotifierProvider.value(value: TextChangeNotifier())],
+            child: MaterialApp(
+                home: MainPage(),
+            ),
+        );
+    }
+}
+
+class MainPage extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+        //透過 Provider.of 來獲取資料
+        final data = Provider.of<TextChangeNotifier>(context);
         return new Scaffold(
             appBar: AppBar(
             title: Text('GetWeatherData'),
             ),
             body: Column(
                 children: <Widget>[
+                    Text(
+                        data.text
+                    ),
                     new RaisedButton(
                         child: Text('Get Weather data'),
-                        onPressed: getWeather,
+                        onPressed: () async {
+                            var map = Map();
+                            map["path"] = "F-C0032-001";
+                            map["token"] = weatherToken;
+                            var res = await HttpUtils.get("/:path?Authorization=:token", map);
+                            Provider.of<TextChangeNotifier>(context, listen: false)
+                                .setData(res.toString());
+                        },
                     )
                 ],
             )
@@ -24,10 +49,22 @@ class GetWeatherData extends StatelessWidget{
 }
 
 // get weather data
-void getWeather() async {
+void getWeather(BuildContext context) async {
     var map = Map();
     map["path"] = "F-C0032-001";
     map["token"] = weatherToken;
     var res = await HttpUtils.get("/:path?Authorization=:token", map);
-    var weatherResult = WeatherResultEntity().fromJson(res);
+    Provider.of<TextChangeNotifier>(context, listen: false)
+        .setData(res.toString());
+//    var weatherResult = WeatherResultEntity().fromJson(res);  // 將 json 轉成一個 bean
+}
+
+class TextChangeNotifier with ChangeNotifier {
+    String _text = "";
+    String get text => _text;
+    setData(String data){
+        _text = data;
+        print("setdata : $_text");
+        notifyListeners();
+    }
 }
